da44198f8de674cbd13d334931613b3aead4af8d
Gap08 <cassylum23@gmail.com>
2023-03-06 10:57:47 +0800

    Create feed_item.dart

---
+++ UI-Clone (Social App)/feed_item.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+
+class FeedItem extends StatelessWidget {
+  final String name;
+  final String content;
+  final String icon;
+
+  const FeedItem(
+      {super.key,
+      required this.name,
+      required this.content,
+      required this.icon});
+
+  @override
+  Widget build(BuildContext context) 
+  {
+    return Padding
+    (
+      padding: const EdgeInsets.fromLTRB(16, 16, 16, 0),
+      child: Container
+      (
+          decoration: BoxDecoration
+          (
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(8),
+          ),
+          height: 120,
+          child: Padding
+          (
+            padding: const EdgeInsets.all(16.0),
+            child: Row
+            (
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: 
+              [
+                SizedBox
+                (
+                  height: 56,
+                  width: 56,
+                  child: ClipRRect
+                  (
+                    borderRadius: BorderRadius.circular(100),
+                    child: Image.network
+                    (
+                      icon,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 16),
+                Expanded
+                (
+                  child: Column
+                  (
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: 
+                    [
+                      Text
+                      (
+                        name,
+                        style: const TextStyle
+                        (
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        ),
+                      ),
+                      Text(content),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          )),
+    );
+  }
+}
